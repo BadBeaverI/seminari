4c8caa7fa151a83bbe5f79045dc87fb17bfccc61
Sergey <sesmbile1@gmail.com>
2023-02-04 09:48:37 +0200

    папки с семинарами и дз

---
+++ seminar/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Укажите номер четверти координат : ");
+int n=Convert.ToInt32(Console.ReadLine());
+if(n==1){
+    Console.WriteLine("X>0 && Y>0");
+}
+else if(n==2){
+    Console.WriteLine("X<0 && Y>0");
+}
+else if(n==3){
+    Console.WriteLine("X<0 && Y<0");
+}
+else if(n==4){
+    Console.WriteLine("X>0 && Y<0");
+}
+else{
+    Console.Write("Вы ошиблись!");
+}
