cb9af0c41a7510c6d92c8404a668697e8cbde1c8
Sergey <sesmbile1@gmail.com>
2023-02-04 18:22:15 +0200

    решаю 3ю задачу- чтоб

--- homework2/zada4a13/zada4a13.cs
+++ homework2/zada4a13/zada4a13.cs
@@ -3,16 +3,26 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Clear();
-Console.Write("ВВЕДИТЕ трехзначное не отрицательное число: ");
+Console.Clear ();
+Console.Write ("Введите не отрицательное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
-if (n<99)
-    {Console.Write("третьей цыфры нет");}
-    else {
-        int n1 = n % 10;
+int n1;
+if (n<100)
+{
+    Console.Write("Третьей цифры нет.");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if ((n/100)<10)
+{
+    n1 = n % 100; 
+    Console.WriteLine ($"третья цифра числа {n1%10}");  
+}
+while (n/100>9)
+{
+    n = n/10;
+    if ((n/100)<10)
+    {
+        n1 = n % 100; 
+        Console.WriteLine ($"третья цифра числа:  {n1%10}");  
     }
-
-Console.Write("третья цифра числа это: ");
-Console.WriteLine(n1);
-    
+}
