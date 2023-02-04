67b937d247cd9f0ef3a916df2c170b1af9fb6d81
Sergey <sesmbile1@gmail.com>
2023-02-04 11:52:57 +0200

    решил 1ю задачу

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -1,18 +1,7 @@
 ﻿Console.Clear();
-Console.Write("Укажите номер четверти координат : ");
-int n=Convert.ToInt32(Console.ReadLine());
-if(n==1){
-    Console.WriteLine("X>0 && Y>0");
-}
-else if(n==2){
-    Console.WriteLine("X<0 && Y>0");
-}
-else if(n==3){
-    Console.WriteLine("X<0 && Y<0");
-}
-else if(n==4){
-    Console.WriteLine("X>0 && Y<0");
-}
-else{
-    Console.Write("Вы ошиблись!");
+
+Console.Write("Введите целое число: ");
+int x = Convert.ToUInt32(Console.ReadLine());
+for(int i = 0;i<=x;i++ ){
+    Console.WriteLine(Math.Pow(i,2));
 }
